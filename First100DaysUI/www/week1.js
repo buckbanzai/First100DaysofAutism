26fc1da60dc442e3d77f192ede93903d8d0bc7b7
Antoine Atallah <aatallah@gmx.net>
2013-03-23 16:39:29 -0700

    Adding new week

---
+++ First100DaysUI/www/week1.js
@@ -0,0 +1,40 @@
+﻿var progress = {
+    fields: [
+            { type: "notes" },
+            { type: "todo" },
+            { type: "inprog" },
+            { type: "completed" },
+            { type: "na" }
+        ]
+}
+
+var week1 = {
+    data: [
+        {
+            header: "Make a doctor's appointment",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
+            subtext: "Some footer note",
+            pagetype: "action",
+            children : [
+                {
+                    header: "Make a doctor's appointment",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
+                    subtext: "Some footer note",
+                    pagetype: "resource",
+                }, 
+                {
+                    header: "Make a doctor's appointment",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
+                    subtext: "Some footer note",
+                    pagetype: "resource",
+                },
+               ]
+
+        },
+        {
+            header: "Something Else",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
+            subtext: "Some footer note"
+    },
+    ]
+}
