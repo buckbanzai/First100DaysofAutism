dcf008b2b9b3f01ce42765edf31a14b9b60d94a7
Alex Weinstein <alex94040@yahoo.com>
2013-03-24 11:05:31 -0700

    Fixing JS file

--- First100DaysUI/www/Actions/About.DiagnosisProcess.1.js
+++ First100DaysUI/www/Actions/About.DiagnosisProcess.1.js
@@ -15,21 +15,21 @@ var page1 = {
 		},
 		{
 			/* This item goes to another page with several child tasks on it */
-			id: "Action_Week_1_Question2"
+			id: "Action_Week_1_Question2",
 			header: "Make an appointment with a specialist",
 			text: "Your pediatrician may recommend appointments with specialists.",
 			page: {
 				id: "makeAppt",
 				header: "Make an appointment",
 				tasks: [ 
-					{ text: "Early intervention specialist"; },
-					{ text: "Developmental pediatrician"; },
-					{ text: "Neurologist"; },
-					{ text: "Psychiatrist"; },
-					{ text: "Psychologist"; },
-					{ text: "Audiologist"; },
-					{ text: "Speech and Language therapist"; },
-					{ text: "Occupational therapist"; }
+					{ text: "Early intervention specialist" },
+					{ text: "Developmental pediatrician" },
+					{ text: "Neurologist" },
+					{ text: "Psychiatrist" },
+					{ text: "Psychologist" },
+					{ text: "Audiologist" },
+					{ text: "Speech and Language therapist" },
+					{ text: "Occupational therapist" }
 				]
 			}
 		}
@@ -37,13 +37,13 @@ var page1 = {
 };
 
 var page2 = {
-	id: "Resource_About_Diagnosis_1";
-	header: "Why does my child need a diagnosis of autism?";
-	text: "<emphasis>You may wonder: why does my child need a diagnosis of autism? That&apos;s a fair question to ask - especially when no one is able to offer you a cure.</emphais>.<br/>A thorough and detailed diagnosis provides important information about your child&apos;s behavior and development. It can help create a roadmap for treatment by identifying your child&apos;s specific strengths and challenges and providing useful information about which needs and skills should be targeted for effective intervention. A diagnosis is often required to access autism-specific services through early intervention programs or your local school district";
+	id: "Resource_About_Diagnosis_1",
+	header: "Why does my child need a diagnosis of autism?",
+	text: "<emphasis>You may wonder: why does my child need a diagnosis of autism? That&apos;s a fair question to ask - especially when no one is able to offer you a cure.</emphais>.<br/>A thorough and detailed diagnosis provides important information about your child&apos;s behavior and development. It can help create a roadmap for treatment by identifying your child&apos;s specific strengths and challenges and providing useful information about which needs and skills should be targeted for effective intervention. A diagnosis is often required to access autism-specific services through early intervention programs or your local school district"
 };
 	
 var page3 = {
-	id: "Resource_About_Diagnosis_2";
-	header: "How is autism diagnosed?";
-	text: "Presently, there is not a medical test for autism; a diagnosis is based on observed behavior and educational and psychological testing. As the symptoms of autism vary, so do the routes to obtaining a diagnosis. You may have raised questions with your pediatrician. Some children are identified as having developmental delays before obtaining a diagnosis of autism and may already receive some early intervention or special education services. Unfortunately, parents’ concerns are sometimes not taken seriously by their doctor and a diagnosis is delayed. Autism Speaks and other autism-related organizations are working hard to educate parents and physicians, so that children with autism are identified as early as possible.<br/>From birth to at least 36 months of age, every child should be screened for developmental milestones during routine visits. The American Academy of Pediatrics recommends that all children be screened for autism at their 18- and 24-month well-baby checkups. If concerns about a child’s development are raised, his or her doctor should refer the child to Early Intervention and a specialist for a developmental evaluation. Lead exposure and hearing screenings should also be performed. Following the results, your child may be referred to a specialist, usually a <strong>Developmental Pediatrician</strong>, a <strong>Neurologist</strong>, a <strong>Psychiatrist</strong> or a <strong>Psychologist</strong>, for further evaluation. The team of specialists that evaluate your child’s condition may also include an Audiologist, to rule out hearing loss, a <strong>Speech & Language Therapist</strong>, to determine language skills and needs, and an <strong>Occupational Therapist</strong> to evaluate physical and motor skills.<br/>A multi-disciplinary evaluation is important for diagnosing autism and other challenges that often accompany autism, such as delays in motor skills. If your child has not been evaluated by a multi-disciplinary team, you will want to make sure further evaluations are conducted so that you can learn as much as possible about your child’s individual strengths and needs. For more information you can visit the Autism Treatment Network.";
+	id: "Resource_About_Diagnosis_2",
+	header: "How is autism diagnosed?",
+	text: "Presently, there is not a medical test for autism; a diagnosis is based on observed behavior and educational and psychological testing. As the symptoms of autism vary, so do the routes to obtaining a diagnosis. You may have raised questions with your pediatrician. Some children are identified as having developmental delays before obtaining a diagnosis of autism and may already receive some early intervention or special education services. Unfortunately, parents’ concerns are sometimes not taken seriously by their doctor and a diagnosis is delayed. Autism Speaks and other autism-related organizations are working hard to educate parents and physicians, so that children with autism are identified as early as possible.<br/>From birth to at least 36 months of age, every child should be screened for developmental milestones during routine visits. The American Academy of Pediatrics recommends that all children be screened for autism at their 18- and 24-month well-baby checkups. If concerns about a child’s development are raised, his or her doctor should refer the child to Early Intervention and a specialist for a developmental evaluation. Lead exposure and hearing screenings should also be performed. Following the results, your child may be referred to a specialist, usually a <strong>Developmental Pediatrician</strong>, a <strong>Neurologist</strong>, a <strong>Psychiatrist</strong> or a <strong>Psychologist</strong>, for further evaluation. The team of specialists that evaluate your child’s condition may also include an Audiologist, to rule out hearing loss, a <strong>Speech & Language Therapist</strong>, to determine language skills and needs, and an <strong>Occupational Therapist</strong> to evaluate physical and motor skills.<br/>A multi-disciplinary evaluation is important for diagnosing autism and other challenges that often accompany autism, such as delays in motor skills. If your child has not been evaluated by a multi-disciplinary team, you will want to make sure further evaluations are conducted so that you can learn as much as possible about your child’s individual strengths and needs. For more information you can visit the Autism Treatment Network."
 };
