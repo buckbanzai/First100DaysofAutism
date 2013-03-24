654d6c733446e1f829faad1df331b000232ca3f1
Antoine Atallah <aatallah@gmx.net>
2013-03-23 17:58:44 -0700

    sdfds

--- First100DaysUI/www/Actions/week1.js
+++ First100DaysUI/www/Actions/week1.js
@@ -4,13 +4,13 @@ var progress = {
             { type: "todo" },
             { type: "inprog" },
             { type: "completed" },
-        ]
+    ]
 }
 
 var week1 = {
     id: "Action.Week.1",
     header: "Week1: Organize and Plan",
-    overlay: "<strong>Why is it important to get started now?</strong> There may be a waiting list for services and evaluations. Make appointments as soon as possible and follow up as needed.     Don&apos;t hesitate to put your name on multiple lists so you can get the earliest appointment possible."
+    overlay: "<strong>Why is it important to get started now?</strong> There may be a waiting list for services and evaluations. Make appointments as soon as possible and follow up as needed.     Don&apos;t hesitate to put your name on multiple lists so you can get the earliest appointment possible.",
     text: "<strong>Schedule Evaluations</strong><br/>If your child has not had a complete workup, schedule the remainder of necessary evaluations.",
     pagetype: "action",
 
@@ -25,26 +25,24 @@ var week1 = {
             pagetype: "task",
         },
         {
-                    /* This item goes to another page with several child tasks on it */
-                    id: "Action.Week.1.Question2"
-                    header: "Make an appointment with a specialist",
-                    text: "",
-                    subtext: "",
-                    pagetype: "action",
+            /* This item goes to another page with several child tasks on it */
+            id: "Action.Week.1.Question2",
+            header: "Make an appointment with a specialist",
+            text: "",
+            subtext: "",
+            pagetype: "action",
                         
-                        children: [
-                        {
-                                   id: "Action.Week.1.Question2.Specialists";
-                                   header: "
-                        }]
-                },
-               ]
-
+            children: [
+            {
+                id: "Action.Week.1.Question2.Specialists",
+                header: "",
+            }]
         },
         {
             header: "Something Else",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
             subtext: "Some footer note"
-    },
+        },
+    ],
 }
 
--- First100DaysUI/www/week1.js
+++ First100DaysUI/www/week1.js
@@ -11,18 +11,21 @@ var progress = {
 var week1 = {
     data: [
         {
+            id: "1",
             header: "Make a doctor's appointment",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
             subtext: "Some footer note",
             pagetype: "action",
             children : [
                 {
+                    id: "1.1",
                     header: "Make a doctor's appointment",
                     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
                     subtext: "Some footer note",
                     pagetype: "resource",
                 }, 
                 {
+                    id: "1.2",
                     header: "Make a doctor's appointment",
                     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
                     subtext: "Some footer note",
@@ -32,6 +35,7 @@ var week1 = {
 
         },
         {
+            id: "2",
             header: "Something Else",
             text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nibh tellus, iaculis vitae blandit id, adipiscing dignissim lectus. Maecenas purus elit, ultricies vel gravida vitae, auctor id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus ac nisi enim, sit amet consequat velit. Morbi ac massa velit, in facilisis sem. Suspendisse ante metus, fermentum nec tempus in, fermentum ac lorem. Fusce eget consequat tortor.",
             subtext: "Some footer note"
