85295284529b8efff19b475f18f5f29b1b925516
Dmitry Frenkel <dima.frenkel@gmail.com>
2013-03-24 12:50:17 -0700

    data-ajax = false. Icons

--- First100DaysUI/www/init.js
+++ First100DaysUI/www/init.js
@@ -1,13 +1,19 @@
 window.currentTeam = 0;
 var firstLaunch = function() {
-    return localStorage.getItem('first100launch') == null;
+    return localStorage.getItem('first100launch') != "no";
 }
+
+var resetFirstLaunch = function() {
+    localStorage.setItem('first100launch', 'yes', { path: '/' });
+}
+
 
 $(document).bind("mobileinit", function () {
     // Make your jQuery Mobile framework configuration changes here!
     $.support.cors = true;
     $.mobile.allowCrossDomainPages = true;
     $.mobile.page.prototype.options.domCache = false;
+    $.mobile.ignoreContentEnabled = true;
 });
 
 if (firstLaunch()) {
